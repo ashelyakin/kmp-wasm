8e70fc9a7bd48b81b81b68d9e39c851e921b60ff
ashelyakin <andrew.shelyakin980@gmail.com>
2026-07-06 16:40:56 +0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/ashelyakin/portfolio/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.ashelyakin.portfolio
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/commonMain/kotlin/com/ashelyakin/portfolio/Platform.kt
@@ -0,0 +1,7 @@
+package com.ashelyakin.portfolio
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
